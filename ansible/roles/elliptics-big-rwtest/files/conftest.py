af0b0aac064b9d13fe59e24265c85ce7b3a728af
Ivan Kuznetsov <kis8P@yandex.ru>
2014-01-14 13:31:22 +0000

    Tests were moved

--- ansible/roles/elliptics-big-rwtest/files/conftest.py
+++
@@ -1,6 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption('--batch_number', type='int', default='50')
-    parser.addoption('--batch_size', type='int', default='50')
-    parser.addoption('--write_timeout', type='int', default='50')
-    parser.addoption('--wait_timeout', type='int', default='60')
-    parser.addoption('--host', type='string', action='append')
