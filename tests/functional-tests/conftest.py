9426812f26184406d552da5790a8bd4d8b65cae5
Ivan Kuznetsov <kis8P@yandex.ru>
2014-01-28 14:22:50 +0000

    Utile modules were added (for tests)

--- tests/functional-tests/conftest.py
+++ tests/functional-tests/conftest.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-#
+
+import sys
+from os.path import abspath, dirname, join
+
+BASE = dirname(dirname(dirname(abspath(__file__))))
+sys.path.insert(0, join(BASE, 'lib'))
+
 #TODO: заменить Exception на соответствующие исключения elliptics'а во всех тестах
 def pytest_addoption(parser):
     parser.addoption('--write_timeout', type='int', default='20')
